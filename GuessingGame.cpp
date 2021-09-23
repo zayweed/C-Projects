fb05679a3d2db6671db921d66735dbbc3af3d4e2
zayweed <91290511+zayweed@users.noreply.github.com>
2021-09-23 14:29:24 -0700

    Add files via upload

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -2,53 +2,40 @@
 
 using namespace std;
 
-bool check_num(string str) {
-  for (int i = 0; i < str.length(); i++) {
-    if (isdigit(str[i]) == false) {
-      return false;
-    }
-  }
-  return true;
-}
-
 int main() {
 
   int b = 0;
-  while (b == 0) {
+  while (b == 0) { //while loop for game that loops when user wants to play again
 
     cout << "welcome to the guessing game, enter your guesses" << endl;
 
     srand(time(NULL));
     int num = rand() % 101;
 
-    string guessa;
+    int guess;
     int a = 0;
     int trys = 0;
     while(a == 0) {
-      cin >> guessa;
-      if (check_num(guessa) == false) {
-        cout << "invalid input, try again" << endl;
-        continue;
-      }
+      cin >> guess;
       trys++;
-      int guess = stoi(guessa);
-      if (guess == num) {
+      if (guess == num) { //breaks out of while loop and displays number of tries
         a = 1;
         cout << "correct!" << endl << "it took you " << trys << " trys" << endl;
       }
-      if (guess > num) {
+      if (guess > num) { //states if guess is too high
         cout << "too high" << endl;
       }
-      if (guess < num) {
+      if (guess < num) { //states if guess is too low
         cout << "too low" << endl;
       }
     }
 
+    //asks if user wants to play again
     cout << "want to play again?" << endl;
     char cont;
     cin >> cont;
 
-    if (cont == 'n') {
+    if (cont == 'n') { //breaks out of game while loop if user doesn't want to play again
       b = 1;
     }
   }
